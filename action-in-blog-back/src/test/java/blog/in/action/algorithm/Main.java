c46bdbcca3a1632610e73905055aeb025823d48c
Junhyunny <kang3966@naver.com>
2021-05-04 09:38:49 +0900

    Docs: 알고리즘 문제 풀이 업로드

--- action-in-blog-back/src/test/java/blog/in/action/algorithm/Main.java
+++ action-in-blog-back/src/test/java/blog/in/action/algorithm/Main.java
@@ -1,107 +1,51 @@
 package blog.in.action.algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
-    static class Point {
+    static void merge(int[] array, int left, int right) {
 
-        private final int x;
-        private final int y;
+        int index = 0;
+        int mid = (left + right) / 2;
+        int[] temp = new int[right - left];
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static class Vector {
-
-        final int dx;
-        final int dy;
-
-        public Vector(int dx, int dy) {
-            this.dx = dx;
-            this.dy = dy;
-        }
-
-        public Vector(Point p1, Point p2) {
-            this.dx = p2.x - p1.x;
-            this.dy = p2.y - p1.y;
-        }
-
-        public double getScalar() {
-            return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-        }
-
-        public Vector reverse() {
-            return new Vector(-1 * dx, -1 * dy);
-        }
-
-        public Vector addVector(Vector v) {
-            return new Vector(this.dx + v.dx, this.dy + v.dy);
-        }
-
-        @Override
-        public String toString() {
-            return "dx: " + dx + ", dy: " + dy;
-        }
-    }
-
-    public static List<Vector> getAllVectorList(List<Point> pointList) {
-        List<Vector> result = new ArrayList<>();
-        if (pointList.size() == 2) {
-            result.add(new Vector(0, 0));
-        }
-        int size = pointList.size();
-        for (int index = 0; index < size; index++) {
-            for (int subIndex = index + 1; subIndex < size; subIndex++) {
-                Vector vector = new Vector(pointList.get(index), pointList.get(subIndex));
-                result.add(vector);
+        int leftIndex = left;
+        int rightIndex = mid;
+        while (leftIndex < mid && rightIndex < right) {
+            if (array[leftIndex] > array[rightIndex]) {
+                temp[index++] = array[leftIndex++];
+            } else {
+                temp[index++] = array[rightIndex++];
             }
         }
-        return result;
+
+        while (leftIndex < mid) {
+            temp[index++] = array[leftIndex++];
+        }
+
+        while (rightIndex < right) {
+            temp[index++] = array[rightIndex++];
+        }
+
+        for (int subIndex = 0; subIndex < temp.length; subIndex++) {
+            array[subIndex + left] = temp[subIndex];
+        }
     }
 
-    public static double getMinSumOfVector(List<Vector> vectorList) {
-        double result = Double.MAX_VALUE;
-        int size = vectorList.size();
-        for (int index = 0; index < size; index++) {
-            for (int subIndex = index + 1; subIndex < size; subIndex++) {
-                Vector vector1 = vectorList.get(index);
-                Vector vector2 = vectorList.get(subIndex);
-                double scalar1 = vector1.addVector(vector2).getScalar();
-                double scalar2 = vector1.reverse().addVector(vector2).getScalar();
-                double scalar3 = vector1.addVector(vector2.reverse()).getScalar();
-                double scalar4 = vector1.reverse().addVector(vector2.reverse()).getScalar();
-                double temp = Math.min(scalar1, scalar2);
-                temp = Math.min(temp, scalar3);
-                temp = Math.min(temp, scalar4);
-                if (result > temp) {
-                    result = temp;
-                }
-            }
+    static void mergeSort(int[] array, int left, int right) {
+        int mid;
+        if (left < right) {
+            mid = (left + right) / 2;
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+            merge(array, left, right);
         }
-        return result;
     }
 
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int times = sc.nextInt();
-        for (int t = 0; t < times; t++) {
-            int N = sc.nextInt();
-            List<Point> pointList = new ArrayList<>();
-            for (int n = 0; n < N; n++) {
-                int x = sc.nextInt();
-                int y = sc.nextInt();
-                pointList.add(new Point(x, y));
-            }
-            List<Vector> vectorList = getAllVectorList(pointList);
-            double answer = getMinSumOfVector(vectorList);
-            System.out.println(answer);
-        }
-        sc.close();
+        int[] array = new int[]{6, 2, 5, 7, 9, 1, 3, 4, 10, 11, 4};
+        mergeSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
     }
 }
--- action-in-blog-back/src/test/java/blog/in/action/algorithm/Solution.java
+++ action-in-blog-back/src/test/java/blog/in/action/algorithm/Solution.java
@@ -1,57 +1,57 @@
 package blog.in.action.algorithm;
 
-class Solution {
-
-    public void restore(int[] lost, int index, int beforeValue) {
-        // 탐색을 마친 후 데이터 값 복구
-        if (index < lost.length && lost[index] == -1) {
-            lost[index] = beforeValue;
-        }
-    }
-
-    // 빌리는데 성공한 학생 수
-    public int reserve(int[] lost, int[] reserve, int curIndex, int canReserveNumer) {
-        int index = 0;
-        int temp = 0;
-        int result = 0;
-        int length = lost.length;
-        for (index = 0; index < length; index++) {
-            if (canReserveNumer == lost[index]) {
-                temp = lost[index];
-                lost[index] = -1;
-                result = 1;
-                break;
+    class Solution {
+    
+        public void restore(int[] lost, int index, int beforeValue) {
+            // 탐색을 마친 후 데이터 값 복구
+            if (index < lost.length && lost[index] == -1) {
+                lost[index] = beforeValue;
             }
         }
-        // 마지막 체크할 학생 수 초과
-        if (curIndex == reserve.length) {
-            restore(lost, index, temp);
-            return result;
-        }
-        int minusCount = reserve(lost, reserve, curIndex + 1, reserve[curIndex] - 1);
-        int plusCount = reserve(lost, reserve, curIndex + 1, reserve[curIndex] + 1);
-        // 탐색을 마친 후 데이터 값 복구
-        restore(lost, index, temp);
-        return (minusCount > plusCount ? minusCount : plusCount) + result;
-    }
-
-    public int solution(int n, int[] lost, int[] reserve) {
-        // 여별의 옷이 있는 학생이 도두맞은 케이스 제거
-        int lostLength = lost.length;
-        int reserveLength = reserve.length;
-        for (int index = 0; index < reserveLength; index++) {
-            for (int subIndex = 0; subIndex < lostLength; subIndex++) {
-                if (lost[subIndex] == reserve[index]) {
-                    lost[subIndex] = -1;
-                    reserve[index] = -2;
+    
+        // 빌리는데 성공한 학생 수
+        public int reserve(int[] lost, int[] reserve, int curIndex, int canReserveNumer) {
+            int index = 0;
+            int temp = 0;
+            int result = 0;
+            int length = lost.length;
+            for (index = 0; index < length; index++) {
+                if (canReserveNumer == lost[index]) {
+                    temp = lost[index];
+                    lost[index] = -1;
+                    result = 1;
                     break;
                 }
             }
+            // 마지막 체크할 학생 수 초과
+            if (curIndex == reserve.length) {
+                restore(lost, index, temp);
+                return result;
+            }
+            int minusCount = reserve(lost, reserve, curIndex + 1, reserve[curIndex] - 1);
+            int plusCount = reserve(lost, reserve, curIndex + 1, reserve[curIndex] + 1);
+            // 탐색을 마친 후 데이터 값 복구
+            restore(lost, index, temp);
+            return (minusCount > plusCount ? minusCount : plusCount) + result;
         }
-        int minusCount = reserve(lost, reserve, 1, reserve[0] - 1);
-        int plusCount = reserve(lost, reserve, 1, reserve[0] + 1);
-        int result = minusCount > plusCount ? minusCount : plusCount;
-        int answer = n - lost.length + result;
-        return answer;
+    
+        public int solution(int n, int[] lost, int[] reserve) {
+            // 여별의 옷이 있는 학생이 도두맞은 케이스 제거
+            int lostLength = lost.length;
+            int reserveLength = reserve.length;
+            for (int index = 0; index < reserveLength; index++) {
+                for (int subIndex = 0; subIndex < lostLength; subIndex++) {
+                    if (lost[subIndex] == reserve[index]) {
+                        lost[subIndex] = -1;
+                        reserve[index] = -2;
+                        break;
+                    }
+                }
+            }
+            int minusCount = reserve(lost, reserve, 1, reserve[0] - 1);
+            int plusCount = reserve(lost, reserve, 1, reserve[0] + 1);
+            int result = minusCount > plusCount ? minusCount : plusCount;
+            int answer = n - lost.length + result;
+            return answer;
+        }
     }
-}
