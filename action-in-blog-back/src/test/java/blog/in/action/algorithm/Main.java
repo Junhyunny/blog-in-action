ada1445b28ba6f7378bfe4d802c37fceb32c9377
Junhyunny <kang3966@naver.com>
2021-05-06 10:07:44 +0900

    Fix: 퀵 정렬 알고리즘 풀이

--- action-in-blog-back/src/test/java/blog/in/action/algorithm/Main.java
+++ action-in-blog-back/src/test/java/blog/in/action/algorithm/Main.java
@@ -4,47 +4,21 @@ import java.util.Arrays;
 
 public class Main {
 
-    static void merge(int[] array, int left, int right) {
-
-        int index = 0;
-        int mid = (left + right) / 2;
-        int[] temp = new int[right - left + 1];
-
-        int leftIndex = left;
-        int rightIndex = mid + 1;
-        while (leftIndex <= mid && rightIndex <= right) {
-            if (array[leftIndex] < array[rightIndex]) {
-                temp[index++] = array[leftIndex++];
-            } else {
-                temp[index++] = array[rightIndex++];
-            }
-        }
-
-        while (leftIndex <= mid) {
-            temp[index++] = array[leftIndex++];
-        }
-
-        while (rightIndex <= right) {
-            temp[index++] = array[rightIndex++];
-        }
-
-        for (int subIndex = 0; subIndex < temp.length; subIndex++) {
-            array[subIndex + left] = temp[subIndex];
-        }
+    static int partition(int[] array, int left, int right) {
+        return 0;
     }
 
-    static void mergeSort(int[] array, int left, int right) {
+    static void quickSort(int[] array, int left, int right) {
         if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(array, left, mid);
-            mergeSort(array, mid + 1, right);
-            merge(array, left, right);
+            int pivot = partition(array, left, right);
+            partition(array, left, pivot);
+            partition(array, pivot + 1, right);
         }
     }
 
     public static void main(String args[]) {
         int[] array = new int[]{6, 2, 5, 7, 9, 1, 3, 4, 10, 11, 4};
-        mergeSort(array, 0, array.length - 1);
+        partition(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array));
     }
 }
