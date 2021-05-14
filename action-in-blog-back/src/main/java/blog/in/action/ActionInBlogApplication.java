293f4abe4957bf0fd4bc01bb1f9be72e1a804948
Junhyunny <kang3966@naver.com>
2021-05-15 01:01:48 +0900

    Test: Filter, Interceptor, AOP 기능 보완 테스트 코드

--- action-in-blog-back/src/main/java/blog/in/action/ActionInBlogApplication.java
+++ action-in-blog-back/src/main/java/blog/in/action/ActionInBlogApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class ActionInBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActionInBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActionInBlogApplication.class, args);
+    }
 
 }
