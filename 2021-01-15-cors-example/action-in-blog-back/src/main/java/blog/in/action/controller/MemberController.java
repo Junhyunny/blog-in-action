8001449685b897e9a3b7291a11a41070604bc4af
Junhyunny <kang3966@naver.com>
2021-07-06 23:45:36 +0900

    Chore: CORS 테스트 코드 정리 및 업로드

---
+++ 2021-01-15-cors-example/action-in-blog-back/src/main/java/blog/in/action/controller/MemberController.java
@@ -0,0 +1,39 @@
+package blog.in.action.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import blog.in.action.annotation.TokenMember;
+import blog.in.action.entity.Member;
+import blog.in.action.service.MemberService;
+
+@RestController
+@RequestMapping(value = "/api/member")
+public class MemberController {
+
+	@Autowired
+	private MemberService memberService;
+
+	@PostMapping("/sign-up")
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void requestSignUp(@RequestBody Member member) {
+		memberService.registMember(member);
+	}
+
+	@GetMapping("/user-info")
+	public Member requestUserInfo(@RequestParam("id") String id) {
+		return memberService.findById(id);
+	}
+
+	@GetMapping("/user-info-using-token")
+	public Member requestUserInfoUsingToken(@TokenMember Member member) {
+		return memberService.findById(member.getId());
+	}
+}
