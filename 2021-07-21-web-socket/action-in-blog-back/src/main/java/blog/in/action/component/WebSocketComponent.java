6f8f08f6b343c7ef4278fcdd1c27837274f983ab
Junhyunny <kang3966@naver.com>
2021-07-26 23:35:30 +0900

    Test: Websocket 테스트 코드

--- 2021-07-21-web-socket/action-in-blog-back/src/main/java/blog/in/action/component/WebSocketComponent.java
+++ 2021-07-21-web-socket/action-in-blog-back/src/main/java/blog/in/action/component/WebSocketComponent.java
@@ -6,7 +6,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -24,17 +23,6 @@ public class WebSocketComponent extends TextWebSocketHandler {
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) {
-        sessionMap.forEach((sessionId, sessionInMap) -> {
-            try {
-                sessionInMap.sendMessage(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
         sessionMap.forEach((sessionId, sessionInMap) -> {
             try {
                 sessionInMap.sendMessage(message);
