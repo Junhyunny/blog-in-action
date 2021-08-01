d10b86952c4f1e86346340409cfeb780cf279b33
Junhyunny <kang3966@naver.com>
2021-08-01 13:02:19 +0900

    Test Auto Boxing 성능 테스트 코드

---
+++ action-in-blog-back/src/test/java/blog/in/action/autoboxing/OptimizationSnoopInt.java
@@ -0,0 +1,14 @@
+package blog.in.action.autoboxing;
+
+public final class OptimizationSnoopInt {
+
+    final Integer id;
+
+    OptimizationSnoopInt(Integer id) {
+        this.id = id;
+    }
+
+    Integer getId() {
+        return id;
+    }
+}
---
+++ action-in-blog-back/src/test/java/blog/in/action/wrapper/WrapperClassTest.java
@@ -0,0 +1,5 @@
+package blog.in.action.wrapper;
+
+public class WrapperClassTest {
+
+}
