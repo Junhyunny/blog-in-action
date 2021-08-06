7df763add539d70d1373d29e5c84e84dcdabd0a9
Junhyunny <kang3966@naver.com>
2021-08-06 11:27:06 +0900

    Test static 하위 폴더 이미지를 못 찾는 현상 정리

---
+++ 2021-08-06-cannot-find-static-resource/action-in-blog-back/src/main/java/blog/in/action/config/WebMvcConfiguration.java
@@ -0,0 +1,9 @@
+package blog.in.action.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+}
---
+++ 2021-08-06-cannot-find-static-resource/action-in-blog-back/src/main/java/blog/in/action/controller/BlogController.java
@@ -0,0 +1,12 @@
+package blog.in.action.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("/api")
+public class BlogController {
+
+}
