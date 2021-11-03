a7b23915f20d3599a37c31d0f81eec7f84cb9577
Junhyunny <kang3966@naver.com>
2021-11-04 08:24:52 +0900

    Test Spring Legacy 코드 업로드

--- 2021-11-03-do-not-bind-proxy-spring-mvc-transaction/action-in-blog-back/src/main/java/blog/in/action/controller/BlogController.java
+++ 2021-11-03-do-not-bind-proxy-spring-mvc-transaction/action-in-blog-back/src/main/java/blog/in/action/controller/BlogController.java
@@ -2,14 +2,19 @@ package blog.in.action.controller;
 
 import blog.in.action.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class BlogController {
 
     @Autowired
     private BlogService blogService;
+
+    @RequestMapping(value = {"", "/"})
+    public String index() {
+        return "index";
+    }
 
     @RequestMapping("/update")
     public void updateBlog() {
