2826935ce600e171b234db710ebfdf461833dc66
Junhyunny <kang3966@naver.com>
2021-11-12 23:56:53 +0900

    Test 쿼츠 클러스터링 테스트 코드 등록

--- 2021-11-07-quartz-in-spring-mvc/action-in-blog-back/src/main/java/blog/in/action/job/BlogJob.java
+++ 2021-11-07-quartz-in-spring-mvc/action-in-blog-back/src/main/java/blog/in/action/job/BlogJob.java
@@ -3,17 +3,19 @@ package blog.in.action.job;
 import blog.in.action.service.BlogService;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 public class BlogJob extends QuartzJobBean {
 
     private BlogService blogService;
 
+    public void setBlogService(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         try {
-            blogService = ((ApplicationContext) jobExecutionContext.getScheduler().getContext().get("applicationContext")).getBean(BlogService.class);
             blogService.updateTest();
         } catch (Exception e) {
             e.printStackTrace();
