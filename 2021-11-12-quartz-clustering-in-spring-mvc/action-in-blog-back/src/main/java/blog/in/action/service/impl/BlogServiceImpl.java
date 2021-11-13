75bab38a06fdca16c4cfcd0d55bc95bc08136dc6
Junhyunny <kang3966@naver.com>
2021-11-13 11:16:49 +0900

    Test Quartz 클러스터링 테스트 코드 작성

--- 2021-11-12-quartz-clustering-in-spring-mvc/action-in-blog-back/src/main/java/blog/in/action/service/impl/BlogServiceImpl.java
+++ 2021-11-12-quartz-clustering-in-spring-mvc/action-in-blog-back/src/main/java/blog/in/action/service/impl/BlogServiceImpl.java
@@ -4,11 +4,13 @@ import blog.in.action.dao.BlogDao;
 import blog.in.action.service.BlogService;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Service;
 
 @Service("blobService")
 public class BlogServiceImpl implements BlogService {
+
+    private Logger logger = Logger.getLogger(BlogServiceImpl.class.getName());
 
     private final BlogDao blogDao;
 
@@ -18,12 +20,10 @@ public class BlogServiceImpl implements BlogService {
 
     @Override
     public void updateTest() {
+        logger.info("update test table");
         List<Map<String, Object>> itemList = blogDao.selectTest();
         for (Map<String, Object> item : itemList) {
             blogDao.updateTest(item);
-            if (new Random().nextBoolean()) {
-                throw new RuntimeException("throw exception");
-            }
         }
     }
 }
