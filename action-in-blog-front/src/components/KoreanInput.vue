e1a0a8b27d448bc9259033f50812cf1ebf68590f
Junhyunny <kang3966@naver.com>
2021-11-17 01:47:24 +0900

    Test Vue.js 한글 입력 버그 해결 방법 추가

---
+++ action-in-blog-front/src/components/KoreanInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>{{this.value}}(길이:{{this.value.length}})</h3>
+        <input @input="changeValue($event)"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'KoreanInput',
+    data() {
+        return {
+            value: ''
+        }
+    },
+    methods: {
+        changeValue(event) {
+            this.value = event.target.value;
+        }
+    }
+}
+</script>
