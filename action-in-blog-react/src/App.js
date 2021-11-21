02bd4ea22b4bfdae634b6ce62eb98573c11920c3
Junhyunny <kang3966@naver.com>
2021-11-21 23:05:11 +0900

    Test React 테스트 코드 업로드

--- action-in-blog-react/src/App.js
+++ action-in-blog-react/src/App.js
@@ -1,9 +1,17 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    const expenses = [
+        { id: 'e1', title: 'Toilet Paper', amount: 67.67, date: new Date(2021, 2, 28) },
+        { id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+    ];
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+        </div>
+    );
 }
 
 export default App;
