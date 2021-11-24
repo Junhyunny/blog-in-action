42e54ae62a60c82a4cf6fba479be7c204361032b
Junhyunny <kang3966@naver.com>
2021-11-25 01:23:07 +0900

    Test Vue.js Computed 속성 사용시 파라미터 전달 포스트 예시 코드 업로드

---
+++ 2021-11-24-vue-js-computed-with-parameter/action-in-blog-front/src/components/ParameterComputed.vue
@@ -0,0 +1,27 @@
+<template>
+    <div >
+        <p>계산된 메시지 with true: "{{ computedMessage(true) }}"</p>
+        <p>계산된 메시지 with false: "{{ computedMessage(false) }}"</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ParameterComputed',
+    data() {
+        return {
+            message: '안녕하세요'
+        }
+    },
+    computed: {
+        computedMessage() {
+            return (flag) => {
+                if (flag) {
+                    return this.message.split('').reverse().join('')
+                }
+                return this.message;
+            }
+        }
+    }
+}
+</script>
