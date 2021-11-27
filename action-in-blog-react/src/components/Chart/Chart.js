757a2d86ff6a51a36aef051f12df61efbdde8307
Junhyunny <kang3966@naver.com>
2021-11-28 01:16:18 +0900

    Chore remove console.log

--- action-in-blog-react/src/components/Chart/Chart.js
+++ action-in-blog-react/src/components/Chart/Chart.js
@@ -7,8 +7,6 @@ const Chart = (props) => {
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
     const totalMaximum = Math.max(...dataPointValues);
 
-    console.log(totalMaximum)
-
     return (
         <div className="chart">
             {props.dataPoints.map((item) => 
