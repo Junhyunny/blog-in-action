0712880ce38160efa8d592efe7324203d46a8df6
Junhyunny <kang3966@naver.com>
2021-11-27 18:04:21 +0900

    Test React practice

---
+++ action-in-blog-react/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value);
+    };
+
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" onChange={titleChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className="new-expense_actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    );
+}
+
+export default ExpenseForm;
---
+++ action-in-blog-react/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import './NewExpense.css';
+
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = () => {
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    );
+}
+
+export default NewExpense;
