441e80b715e8deb1c310547ce3c73c2df40cb6e1
Junhyunny <kang3966@naver.com>
2021-11-28 00:02:30 +0900

    Feat add ExpenseList component

--- action-in-blog-react/src/App.js
+++ action-in-blog-react/src/App.js
@@ -6,37 +6,24 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
 
     const [expenses, setExpenses] = useState([
-        { id: 'e1', title: 'Toilet Paper', amount: 67.67, date: new Date(2021, 2, 28), display: true },
-        { id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28), display: true },
+        { id: 'e1', title: 'Toilet Paper', amount: 67.67, date: new Date(2021, 2, 28) },
+        { id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
     ]);
-    
-    const [year, setYear] = useState('2021');
     
     const onSaveExpenseData = (expenseData) => {
         const newExpenseData = {
             ...expenseData,
             id: Math.random().toString(),
-            display: expenseData.date.getFullYear().toString() === year
         }
         setExpenses((preExpense) => {
             return [newExpenseData, ...preExpense];
         });
     };
 
-    const yearChangeHandler = (selectedYear) => {
-        setYear(selectedYear);
-        setExpenses((preExpense) => {
-            return [...preExpense.map((expense) => {
-                expense.display = expense.date.getFullYear().toString() === selectedYear;
-                return expense;
-            })];
-        });
-    };
-
     return (
         <div>
             <NewExpense onSaveExpenseData={onSaveExpenseData}/>
-            <Expenses items={expenses}  onYearChangeHandler= {yearChangeHandler} />
+            <Expenses items={expenses}/>
         </div>
     );
 }
--- action-in-blog-react/src/components/Expenses/Expenses.js
+++ action-in-blog-react/src/components/Expenses/Expenses.js
@@ -1,22 +1,25 @@
-import ExpenseItem from "./ExpenseItem";
+import { useState } from "react";
+
+import ExpenseList from "./ExpenseList";
 import ExpenseFilter from "./ExpenseFilter";
 import './Expenses.css';
 
 const Expenses = (props) => {
+
+    const [year, setYear] = useState('2021');
+
+    const yearChangeHandler = (selectedYear) => {
+        setYear(selectedYear);
+    };
+
+    const filteredItems = props.items.filter((item) => {
+        return item.date.getFullYear().toString() === year;
+    });
+
     return (
         <div className="expenses">
-            <ExpenseFilter onYearChangeHandler={props.onYearChangeHandler}/>
-            {
-                props.items
-                    .filter((item) => item.display)
-                    .map((item) => (
-                        <ExpenseItem 
-                            key={item.id}
-                            title={item.title} 
-                            amount={item.amount} 
-                            date={item.date} />
-                ))
-            }
+            <ExpenseFilter onYearChangeHandler={yearChangeHandler} />
+            <ExpenseList items={filteredItems}/>
         </div>
     );
 };
