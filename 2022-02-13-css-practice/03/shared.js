3171fc4bd77fd6e21bb9fd049cab9f4de66c0750
Junhyunny <kang3966@naver.com>
2022-02-13 16:49:11 +0900

    Add CSS Practice 업로드

---
+++ 2022-02-13-css-practice/03/shared.js
@@ -0,0 +1,45 @@
+const body = document.body;
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector('.modal');
+const yesButtonInModal = document.querySelector('a.modal__action');
+const noButtonInModal = document.querySelector('.modal__action--negative');
+const buttons = document.querySelectorAll('.plan button');
+
+const openModal = () => {
+    // backdrop.className = 'open'; // this will actually overwrite the complete class
+    body.classList.add('prevent-scroll');
+    backdrop.classList.add('open');
+    modal.classList.add('open');
+};
+
+const closeModal = () => {
+    body.classList.remove('prevent-scroll');
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
+};
+
+buttons.forEach(button => {
+    button.addEventListener('click', openModal);
+});
+backdrop.addEventListener('click', closeModal);
+yesButtonInModal.addEventListener('click', closeModal);
+noButtonInModal.addEventListener('click', closeModal);
+
+// 햄버거 메뉴
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
+const openMobileNav = () => {
+    body.classList.add('prevent-scroll');
+    backdrop.classList.add('open');
+    mobileNav.classList.add('open');
+};
+
+const closeMobileNav = () => {
+    body.classList.remove('prevent-scroll');
+    backdrop.classList.remove('open');
+    mobileNav.classList.remove('open');
+};
+
+toggleButton.addEventListener('click', openMobileNav);
+backdrop.addEventListener('click', closeMobileNav);
