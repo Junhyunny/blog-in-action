d2fb9ad8e43a7228d16d15a0f0cfe74a367e7d0c
Junhyunny <kang3966@naver.com>
2022-06-06 04:15:29 +0900

    Test FeignClient with sleuth 테스트

--- 2022-06-04-logging-trace-with-sleuth-in-msa/action-in-blog/a-service/src/main/java/blog/in/action/client/AClient.java
+++ 2022-06-04-logging-trace-with-sleuth-in-msa/action-in-blog/a-service/src/main/java/blog/in/action/client/AClient.java
@@ -1,7 +1,11 @@
 package blog.in.action.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient
 public interface AClient {
+
+    @GetMapping("b-path")
+    String getDataFromBService();
 }
--- 2022-06-04-logging-trace-with-sleuth-in-msa/action-in-blog/a-service/src/main/java/blog/in/action/controller/ASleuthController.java
+++ 2022-06-04-logging-trace-with-sleuth-in-msa/action-in-blog/a-service/src/main/java/blog/in/action/controller/ASleuthController.java
@@ -1,10 +1,15 @@
 package blog.in.action.controller;
 
+import blog.in.action.client.AClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class ASleuthController {
+
+    private final AClient aClient;
 
     @GetMapping("/a-path")
     public String aPath() {
