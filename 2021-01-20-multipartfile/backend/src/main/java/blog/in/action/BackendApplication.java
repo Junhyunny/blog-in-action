cc19c66338d5f782a5c044426a742582b38de2d4
Junhyunny <kang3966@naver.com>
2022-06-23 12:05:28 +0900

    Chore change test

--- 2021-01-20-multipartfile/backend/src/main/java/blog/in/action/BackendApplication.java
+++ 2021-01-20-multipartfile/backend/src/main/java/blog/in/action/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ActionInBlogApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ActionInBlogApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
