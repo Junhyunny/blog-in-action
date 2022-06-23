3d47d2c3bb75a981c39ffe32327f4c09cc402c6d
Junhyunny <kang3966@naver.com>
2022-06-23 13:46:16 +0900

    Chore Change 프로젝트 이름

---
+++ 2022-03-13-react-test-environment/action-in-blog/src/App.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
+import userEvent from '@testing-library/user-event'
+
+describe('App', () => {
+    it('renders App', () => {
+        render(<App />)
+
+        expect(screen.getByText('Hello Vite + React!')).toBeInTheDocument()
+    })
+
+    it('click count button', async () => {
+        render(<App />)
+
+        userEvent.click(screen.getByText(/count is: /i))
+
+        await waitFor(() => {
+            expect(screen.getByText('count is: 1')).toBeInTheDocument()
+        })
+    })
+})
