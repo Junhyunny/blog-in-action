d6a2de6cdad07990e35f6e2ea8518bcd9549237e
Junhyunny <kang3966@naver.com>
2022-09-30 11:51:04 +0900

    Change nginx.conf and add logs

--- 2022-09-30-docker-network/backend/src/main/java/action/in/blog/todo/Todo.java
+++ 2022-09-30-docker-network/backend/src/main/java/action/in/blog/todo/Todo.java
@@ -8,4 +8,9 @@ import lombok.Setter;
 public class Todo {
 
     private String title;
+
+    @Override
+    public String toString() {
+        return "{title: " + title + "}";
+    }
 }
--- 2022-09-30-docker-network/backend/src/main/java/action/in/blog/todo/service/DefaultTodoService.java
+++ 2022-09-30-docker-network/backend/src/main/java/action/in/blog/todo/service/DefaultTodoService.java
@@ -1,10 +1,14 @@
 package action.in.blog.todo.service;
 
 import action.in.blog.todo.Todo;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Log4j2
+@Service
 public class DefaultTodoService implements TodoService {
 
     private List<Todo> todos = new ArrayList<>();
@@ -14,6 +18,7 @@ public class DefaultTodoService implements TodoService {
         synchronized (todos) {
             todos.add(todo);
         }
+        log.info(todos);
     }
 
     @Override
