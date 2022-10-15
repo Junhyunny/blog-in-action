704766cb36d98ee535eb303b907914f75b63ae87
Junhyunny <kang3966@naver.com>
2022-10-15 20:10:15 +0900

    Add react context 설정 변경

--- 2022-10-14-react-context/frontend/src/App.test.js
+++ 2022-10-14-react-context/frontend/src/App.test.js
@@ -3,41 +3,35 @@ import {render, screen} from "@testing-library/react";
 import React from "react";
 
 import App from "./App";
-
-import PokeProvider, {PokeContext} from "./store/PokeProvider";
-import UIProvider, {UIContext} from "./store/UIProvider";
-
-jest.mock('react', () => ({
-    ...jest.requireActual('react'), useContext: (context) => {
-        console.log(context)
-        if (typeof context === 'PokeContext') {
-            return {
-                state: {
-                    pokemons: [{name: 'bulbasaur'}, {name: 'ivysaur'}]
-                }, dispatch: jest.fn()
-            }
-        } else if (typeof context === 'UIContext') {
-            return {
-                state: {
-                    isLoading: false
-                }, dispatch: jest.fn()
-            }
-        }
-    },
-}))
+import react from "react";
 
 describe('App Tests', () => {
 
-    test('renders pokemon lists', () => {
+    test('renders pokemons', () => {
+        jest.spyOn(react, 'useContext').mockReturnValue({
+            state: {
+                pokemons: [{name: 'bulbasaur'}, {name: 'ivysaur'}],
+                isLoading: false
+            }
+        })
 
-        render(<UIProvider>
-            <PokeProvider>
-                <App/>
-            </PokeProvider>
-        </UIProvider>)
+        render(<App/>)
 
         expect(screen.getByText('bulbasaur')).toBeInTheDocument()
         expect(screen.getByText('ivysaur')).toBeInTheDocument()
     });
-})
 
+    test('renders other pokemons', () => {
+        jest.spyOn(react, 'useContext').mockReturnValue({
+            state: {
+                pokemons: [{name: 'kangaskhan'}, {name: 'horsea'}],
+                isLoading: false
+            }
+        })
+
+        render(<App/>)
+
+        expect(screen.getByText('kangaskhan')).toBeInTheDocument()
+        expect(screen.getByText('horsea')).toBeInTheDocument()
+    });
+})
