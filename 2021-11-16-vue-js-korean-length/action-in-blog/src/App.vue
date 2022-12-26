40af18f69b4240d36312f617f75cacb49d0dd8b4
JUNHYUN KANG <junhyunk@junhyunkEMD6T.vmware.com>
2022-12-27 00:23:25 +0900

    Add Vue 한글 입력 예제 코드 업데이트

--- 2021-11-16-vue-js-korean-length/action-in-blog/src/App.vue
+++ 2021-11-16-vue-js-korean-length/action-in-blog/src/App.vue
@@ -1,14 +1,38 @@
 <template>
-    <KoreanInput />
+  <KoreanInput/>
 </template>
 
 <script>
 import KoreanInput from './components/KoreanInput.vue'
 
 export default {
-    name: 'App',
-    components: {
-        KoreanInput
-    }
+  name: 'App',
+  components: {
+    KoreanInput
+  }
 }
 </script>
+
+<style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  margin: 1rem;
+  box-sizing: border-box;
+}
+</style>
--- 2021-11-16-vue-js-korean-length/action-in-blog/src/components/KoreanInput.vue
+++ 2021-11-16-vue-js-korean-length/action-in-blog/src/components/KoreanInput.vue
@@ -1,22 +1,44 @@
 <template>
-    <div>
-        <h3>{{this.value}}(길이:{{this.value.length}})</h3>
-        <input @input="changeValue($event)"/>
+  <div>
+    <input @input="changeValue($event)"/>
+    <div class="header">
+      <h3>{{ value }}</h3>
+      <h4>(길이:{{ value.length }})</h4>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'KoreanInput',
-    data() {
-        return {
-            value: ''
-        }
-    },
-    methods: {
-        changeValue(event) {
-            this.value = event.target.value;
-        }
+  name: 'KoreanInput',
+  data() {
+    return {
+      value: ''
     }
+  },
+  methods: {
+    changeValue(event) {
+      this.value = event.target.value;
+    }
+  }
 }
 </script>
+
+<style scoped lang="css">
+input{
+  margin: 0.25rem 0.5rem;
+}
+
+.header {
+  margin: 0.25rem 0.5rem;
+  padding: 0.5rem;
+  border: 1px lightgray solid;
+}
+
+.header h3 {
+  border-bottom: 1px lightgray solid;
+  height: 2.5rem;
+  display: flex;
+  align-items: center;
+}
+</style>
