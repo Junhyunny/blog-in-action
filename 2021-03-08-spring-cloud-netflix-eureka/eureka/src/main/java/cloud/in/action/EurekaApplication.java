8ef03c949bb2735c1c8b39720c3caaf66466162a
Junhyunny <kang3966@naver.com>
2023-02-05 01:10:23 +0900

    Change Example

--- 2021-03-08-spring-cloud-netflix-eureka/eureka/src/main/java/cloud/in/action/EurekaApplication.java
+++ 2021-03-08-spring-cloud-netflix-eureka/eureka/src/main/java/cloud/in/action/EurekaApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 
 }
