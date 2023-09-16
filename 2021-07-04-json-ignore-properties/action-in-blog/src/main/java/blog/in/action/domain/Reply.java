ff865c13486846567fc107f076559700e8eaeef3
Junhyunny <kang3966@naver.com>
2023-09-16 14:08:48 +0900

    Add reply object

--- 2021-07-04-json-ignore-properties/action-in-blog/src/main/java/blog/in/action/domain/Reply.java
+++ 2021-07-04-json-ignore-properties/action-in-blog/src/main/java/blog/in/action/domain/Reply.java
@@ -1,11 +1,12 @@
 package blog.in.action.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 
 public record Reply(
         long id,
         String content,
-//        @JsonIgnoreProperties(value = "replies")
+        @JsonIgnoreProperties(value = "replies")
         Post post
 ) {
     @Builder
