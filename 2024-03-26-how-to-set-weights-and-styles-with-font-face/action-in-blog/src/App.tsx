d2589485ff9a3faa69fe1a3628adb4b491b23d69
Junhyunny <kang3966@naver.com>
2024-03-26 23:53:28 +0900

    Add how to use font-face directive

---
+++ 2024-03-26-how-to-set-weights-and-styles-with-font-face/action-in-blog/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p className="u400">Hello World(Regular)</p>
+      <p className="u500">Hello World(Medium)</p>
+      <p className="u600">Hello World(SemiBold)</p>
+      <p className="u700">Hello World(Bold)</p>
+    </div>
+  );
+}
+
+export default App;
