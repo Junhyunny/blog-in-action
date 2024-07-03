26790ef497aecf96aec139d52e527222f8ea98bd
Junhyunny <kang3966@naver.com>
2024-07-03 14:07:25 +0900

    Change example code for read resource file

--- 2022-04-09-when-run-jar-then-fail-to-read-resource/action-in-blog/src/main/java/action/in/blog/controller/PokeomonController.java
+++ 2022-04-09-when-run-jar-then-fail-to-read-resource/action-in-blog/src/main/java/action/in/blog/controller/PokeomonController.java
@@ -1,37 +1,39 @@
 package action.in.blog.controller;
 
 import action.in.blog.domain.PokemonResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import java.util.Objects;
 
 @RestController
 public class PokeomonController {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final static ClassLoader classLoader = PokeomonController.class.getClassLoader();
+    private final static ObjectMapper objectMapper = new ObjectMapper();
 
     private PokemonResponse getPokemonResponse(BufferedReader reader) throws IOException {
-        StringBuffer buffer = new StringBuffer();
+        StringBuilder buffer = new StringBuilder();
         String line;
         while ((line = reader.readLine()) != null) {
             buffer.append(line);
         }
-        return objectMapper.readValue(buffer.toString(), PokemonResponse.class);
+        return objectMapper.readValue(
+                buffer.toString(),
+                PokemonResponse.class
+        );
     }
 
     @GetMapping("/pokemons-fail")
     public PokemonResponse getPokemonsFail() {
-        String filePath = this.getClass().getClassLoader().getResource("pokemons.json").getFile();
+        String filePath = Objects.requireNonNull(classLoader.getResource("pokemons.json")).getFile();
         try (
                 FileReader fileReader = new FileReader(filePath);
                 BufferedReader reader = new BufferedReader(fileReader)
         ) {
             return getPokemonResponse(reader);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -40,12 +42,10 @@ public class PokeomonController {
     @GetMapping("/pokemons-success")
     public PokemonResponse getPokemonsSuccess() {
         try (
-                InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("pokemons.json");
+                InputStream inputStream = classLoader.getResourceAsStream("pokemons.json");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))
         ) {
             return getPokemonResponse(reader);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
