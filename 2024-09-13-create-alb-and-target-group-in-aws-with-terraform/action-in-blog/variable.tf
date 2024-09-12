ba3bb4d1d230663868c0753a0c09a12789735ac2
Junhyunny <kang3966@naver.com>
2024-09-13 05:47:44 +0900

    Add base code for creating alb examples

---
+++ 2024-09-13-create-alb-and-target-group-in-aws-with-terraform/action-in-blog/variable.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  default = "us-east-1"
+  type    = string
+}
+
+variable "vpc_name" {
+  default = "project-vpc"
+  type    = string
+}
+
+variable "project_name" {
+  default = "project"
+  type    = string
+}
