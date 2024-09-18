2c8b1d6db9858d22caf9566c5f8b136d33a13b44
Junhyunny <kang3966@naver.com>
2024-09-18 10:22:24 +0900

    Add Deploy EC2 container into target group

---
+++ 2024-09-18-deploy-ec2-into-target-group-in-aws-with-terraform/action-in-blog/subnet.tf
@@ -0,0 +1,27 @@
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.junhyunny_vpc.id
+  cidr_block        = "10.0.0.0/20"
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "${var.vpc_name}-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id                  = aws_vpc.junhyunny_vpc.id
+  cidr_block              = "10.0.16.0/20"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.junhyunny_vpc.id
+  cidr_block        = "10.0.32.0/20"
+  availability_zone = "${var.region}b"
+  tags = {
+    Name = "${var.vpc_name}-public-subnet-2"
+  }
+}
