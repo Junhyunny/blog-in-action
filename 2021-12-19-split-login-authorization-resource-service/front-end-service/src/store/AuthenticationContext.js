5d107f3a5def3982f22ca62d5b0e4d9b6805b91b
Junhyunny <kang3966@naver.com>
2025-02-27 15:38:30 +0900

    Remove 삭제한 글 예제

--- 2021-12-19-split-login-authorization-resource-service/front-end-service/src/store/AuthenticationContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const AuthenticationContext = React.createContext({
-    authenticate: false,
-    setAuthenticate: (authenticate) => {},
-});
-
-export default AuthenticationContext;
