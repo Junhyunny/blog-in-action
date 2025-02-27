5d6ff19daec2614cdd4aad0b06ce9b53fa979283
Junhyunny <kang3966@naver.com>
2025-02-27 17:44:17 +0900

    Remove 토큰 강화기 예제 삭제

--- 2021-01-10-token-enhancer/action-in-blog-resource/src/main/java/blog/in/action/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package blog.in.action.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import blog.in.action.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String>{
-
-}
