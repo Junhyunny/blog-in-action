9d1186b11b1468672ffc34ea6fb6e9d447941bee
Junhyunny <kang3966@naver.com>
2026-05-26 09:15:06 +0900

    Fix spring application context event docker run script

--- 2021-08-17-async-in-spring-application-context-event/action-in-blog/run-db.sh
+++ 2021-08-17-async-in-spring-application-context-event/action-in-blog/run-db.sh
@@ -3,6 +3,6 @@
 docker run -d \
   --name mysql-test \
   -p 3306:3306 \
-  -e MYSQL_ROOT_PASSWORD=1234 \
+  -e MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD:?Set MYSQL_ROOT_PASSWORD}" \
   -e MYSQL_DATABASE=test \
   mysql:8.0
